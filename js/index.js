09b9b586062a86f3639ff6f3826955f37d7fbf35
m_moreno <m_moreno@cartong.org>
2018-07-31 03:26:32 +0200

    [added] lib/cartong/events.js : event handler plugin [added] lib/jquery/jquery-3.3.1.min.js : switch jquery [added] mapstyles/* : playing with custom styles [added] mbtiles/* : playing with custom mbtiles [modified] index.html : added new resources [modified] js/index.js : playing with custom mbtiles and styles

--- js/index.js
+++ js/index.js
@@ -4,12 +4,16 @@
 //TODO: create own vector tiles & styles... uff
 //TODO:
 
+var app = new CartONG.Events();
 var map;
 
 $(function() {
   map = new mapboxgl.Map({
     container: 'map', // id of HTML element containing the map
-    style: 'https://openmaptiles.github.io/osm-bright-gl-style/style-cdn.json', // Style location
+    //style: 'https://openmaptiles.github.io/osm-bright-gl-style/style-cdn.json', // Style location
+    //style: 'http://localhost:8080/mapstyles/osm-bright-gl-custom/osm-bright-larabanga_0.2.json', // Style location
+    style: 'http://localhost:8080/mapstyles/mapbox-streets-custom/mapbox-streets-custom_0.1.json', // Style location
+    //style: 'mapbox://styles/mapbox/streets-v9', // token required
     center: [-1.860, 9.218], // initial map center
     zoom: 15, // initial zoom level
     bearing: 0, // Initial rotation angle
@@ -27,4 +31,66 @@ $(function() {
   var sidebar = turboSidebar('sidebar');
 
   //var sidebar = L.control.sidebar('sidebar').addTo(map);
+
+  /*
+  getSources().done(function(sources) {  
+    for (var key in sources) {
+      map.addSource(key, {
+        type: 'geojson',
+        data: sources[key].geojson,
+      });
+    }
+    
+    for (var i=0; i < geojsonStyles.length; i++) {
+      map.addLayer(geojsonStyles[i]);
+    }
+  });
+  */
+
+  //map.on('mousemove', function (e) {
+  //  var features = map.queryRenderedFeatures(e.point);
+  //  
+  //});
+
 });
+
+function getSources() {
+  var promise = $.Deferred();
+  var source;
+  var remaining = 0;
+  var sources = {};
+
+  //for (var i=0; i < geojsonSources.length; i++) {
+  for (var key in geojsonSources) {
+    remaining ++;
+    var source = geojsonSources[key];
+    loadGeojsonSource(source.url).done(function(k, s) {
+      return function(geojson) {
+        remaining--;
+        s.geojson = geojson;
+        sources[k] = s;
+        if (!remaining) {
+          promise.resolve(sources);
+        }
+      };
+    }(key, source));
+
+  }
+
+  return promise;
+}
+
+function loadGeojsonSource(url) {
+  this.promise = $.Deferred();
+  var geojson = {};
+  if (url === undefined) {
+    promise.resolve(geojson); // should be reject() but listeners should not have to deal with missing geojson
+  } else {
+    $.getJSON(url).done(function(geojson) {
+      promise.resolve(geojson);
+    }).fail(function(err) {
+      promise.resolve(geojson); // should be reject() but listeners should not have to deal with missing geojson
+    });
+  }
+  return promise;
+}
